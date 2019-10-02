f438bdacf6834d37bb3f9b1bcbe01ebd28b5dc0c
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-10-02 00:28:56 -0300

    Alterações na tela de lanches

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -17,24 +17,32 @@ class SecondScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("COMIDAS", style: TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16
-                ),),
+                Text(
+                  "COMIDAS",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16),
+                ),
                 Row(
                   children: <Widget>[
-                    Text("IFSul Campus Camaquã", style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
-                    ),),
-                    Icon(Icons.keyboard_arrow_down, color: Colors.green,)
+                    Text(
+                      "IFSul Campus Camaquã",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.green,
+                    )
                   ],
                 )
               ],
             ),
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 14),
             child: Row(
@@ -43,25 +51,32 @@ class SecondScreen extends StatelessWidget {
                   child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(5),
-                        color: Colors.grey.shade100
-                    ),
+                        color: Colors.grey.shade100),
                     child: TextField(
                       decoration: InputDecoration(
                           hintText: 'Lanches Cantina',
                           border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search, color: Colors.green,)
-                      ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.green,
+                          )),
                     ),
                   ),
                 ),
-                SizedBox(width: 14,),
-                Text('Filtros', style: TextStyle(
-                    color: Colors.green, fontWeight: FontWeight.bold
-                ),)
+                SizedBox(
+                  width: 14,
+                ),
+                Text(
+                  'Filtros',
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
+                )
               ],
             ),
           ),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           Container(
             height: 100,
             decoration: BoxDecoration(
@@ -72,7 +87,6 @@ class SecondScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 Container(
-                  //width: 150,
                   margin: EdgeInsets.symmetric(horizontal: 1),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -89,20 +103,18 @@ class SecondScreen extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 5),
-                      Text('Confira nossas delicias',
+                      Text(
+                        'Confira nossas delicias',
                         style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black54
-                        ),
+                            fontWeight: FontWeight.w500, color: Colors.black54),
                       )
                     ],
                   ),
                 ),
                 SizedBox(
-                  width: 10,
+                  width: 14,
                 ),
                 Container(
-                  //width: 150,
                   margin: EdgeInsets.symmetric(horizontal: 5),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -119,14 +131,16 @@ class SecondScreen extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 5),
-                      Text('Não perca as promoções',
+                      Text(
+                        'Promoções deliciosas',
                         style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black54
-                        ),
+                            fontWeight: FontWeight.w500, color: Colors.black54),
                       )
                     ],
                   ),
+                ),
+                SizedBox(
+                  width: 10,
                 ),
                 Container(
                   margin: EdgeInsets.symmetric(horizontal: 5),
@@ -145,17 +159,19 @@ class SecondScreen extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 5),
-                      Text('Uma explosoes de sabores',
+                      Text(
+                        'Uma explosão de sabores',
                         style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black54
-                        ),
+                            fontWeight: FontWeight.w500, color: Colors.black54),
                       )
                     ],
                   ),
                 ),
               ],
             ),
+          ),
+          SizedBox(
+            height: 5,
           ),
           Container(
             height: 150,
@@ -164,11 +180,13 @@ class SecondScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('Categorias', style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17),
+                Text(
+                  'Categorias',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Expanded(
                   child: ListView(
                     scrollDirection: Axis.horizontal,
@@ -181,18 +199,21 @@ class SecondScreen extends StatelessWidget {
                               child: Image.asset(
                                 'images/xis.jpeg',
                                 height: 70,
-                              )
+                              )),
+                          SizedBox(
+                            height: 7,
                           ),
-                          SizedBox(height: 7,),
-                          Text('Lanches',
+                          Text(
+                            'Lanches',
                             style: TextStyle(
                                 fontWeight: FontWeight.w500,
-                                color: Colors.black54
-                            ),
+                                color: Colors.black54),
                           )
                         ],
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
@@ -201,18 +222,21 @@ class SecondScreen extends StatelessWidget {
                               child: Image.asset(
                                 'images/refrigerante.jpg',
                                 height: 70,
-                              )
+                              )),
+                          SizedBox(
+                            height: 7,
                           ),
-                          SizedBox(height: 7,),
-                          Text('Refrigerantes',
+                          Text(
+                            'Refrigerantes',
                             style: TextStyle(
                                 fontWeight: FontWeight.w500,
-                                color: Colors.black54
-                            ),
+                                color: Colors.black54),
                           )
                         ],
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
@@ -221,27 +245,41 @@ class SecondScreen extends StatelessWidget {
                               child: Image.asset(
                                 'images/cafe.jpg',
                                 height: 70,
-                              )
+                              )),
+                          SizedBox(
+                            height: 7,
                           ),
-                          SizedBox(height: 7,),
-                          Text('Café',
+                          Text(
+                            'Café',
                             style: TextStyle(
                                 fontWeight: FontWeight.w500,
-                                color: Colors.black54
-                            ),
+                                color: Colors.black54),
                           )
                         ],
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 10,
+                      ),
                     ],
                   ),
                 )
               ],
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
           Container(
             height: 10,
             color: Colors.grey.shade100,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Campus Camaquã',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            textAlign: TextAlign.center,
           ),
           Container(
             padding: EdgeInsets.all(12),
@@ -249,55 +287,54 @@ class SecondScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(5),
               child: Image.asset('images/ifsul1.jpg'),
             ),
-          )
+          ),
+          SizedBox(
+            height: 10,
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          elevation: 4,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.black,),
-                title: Text(
-                  'Inicio',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black54
-                  ),
-                )
+      bottomNavigationBar: BottomNavigationBar(elevation: 4, items: [
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.black,
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search, color: Colors.black,),
-                title: Text(
-                  'Busca',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black54
-                  ),
-                )
+            title: Text(
+              'Inicio',
+              style:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.receipt, color: Colors.black,),
-                title: Text(
-                  'Pedidos',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black54
-                  ),
-                )
+            title: Text(
+              'Busca',
+              style:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.receipt,
+              color: Colors.black,
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline, color: Colors.black,),
-                title: Text(
-                  'Perfil',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black54
-                  ),
-                )
+            title: Text(
+              'Pedidos',
+              style:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+              color: Colors.black,
             ),
-          ]
-      ),
+            title: Text(
+              'Perfil',
+              style:
+                  TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+            )),
+      ]),
     );
   }
 }
-
